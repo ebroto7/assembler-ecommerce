ef9364fff79921fa51dd412fe914c7aa845a15bc
Enric Broto Hernandez <enrique7siete@gmail.com>
2023-10-03 18:03:43 +0200

    (add) json-server &api call

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,43 +1,55 @@
 import { useContext, useEffect, useState } from 'react'
 import BookInlineContainer from '../Components/bookInlineContainer/BookInlineContainer'
-import booksList from '../Assets/db/db'
 import { BookType } from '../Types/book'
 import { Link } from 'react-router-dom'
-Navbar
 
 
-import {IoBagCheckOutline } from "react-icons/io5"
- import { CartContext } from '../context/BookContext'
+import { IoBagCheckOutline } from "react-icons/io5"
+import { CartContext } from '../context/BookContext'
 import Navbar from '../Components/Navbar'
 
-const bookFilters = ['Best seller', 'Best rated', 'Wish list' ]
- 
+const bookFilters = ['Best seller', 'Best rated', 'Wish list']
+
 // type Props = {}
 
 export const HomePage = () => {
-    const bookList: BookType[] = booksList
 
-       const { cartItems } = useContext(CartContext)
+  // const { cartItems } = useContext(CartContext)
+  // const [cartNumber, setCartNumber] = useState<number>(cartItems.length)
 
-      const [cartNumber, setCartNumber] = useState<number>(cartItems.length)
+  const [books, setBooks] = useState([])
+  useEffect(() => {
+    const url = 'http://localhost:3000/books'
+    const getProducts = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("json data= " + data)
+      setBooks(data)
+    }
+    getProducts();
 
-      useEffect(() => {
-        setCartNumber(cartItems.length)
-      }, [])
+  }, [])
+
+
+  useEffect(() => {
+    // setCartNumber(cartItems.length)
+  }, [])
 
   return (
     <>
       <Navbar />
       <main className='main'>
-          <Link to="/cart"key ="cart">
-              <button className='homePage_gotocart_Btn'>
-                <IoBagCheckOutline />
-                <p>{cartNumber}</p> 
-              </button>
-          </Link>   
-          {bookFilters.map((filter) => (
-            <BookInlineContainer key={filter} title={filter} bookList={bookList}/>
-          ))}
+        <Link to="/cart" key="cart">
+          <button className='homePage_gotocart_Btn'>
+            <IoBagCheckOutline />
+            {/* <p>{cartNumber}</p>  */}
+          </button>
+        </Link>
+
+
+        {bookFilters.map((filter) => (
+          <BookInlineContainer key={filter} title={filter} bookList={books} />
+        ))}
       </main>
     </>
   )
