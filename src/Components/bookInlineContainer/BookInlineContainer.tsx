33046962b5fe8062ed7ed59d912dddbbc915a0ee
Enric Broto Hernandez <enrique7siete@gmail.com>
2023-10-06 17:33:54 +0200

    (add) function for shuffle arrays

--- src/Components/bookInlineContainer/BookInlineContainer.tsx
+++ src/Components/bookInlineContainer/BookInlineContainer.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { BookType } from '../../Types/book';
 import BookCard from '../bookCard/BookCard';
+import { shuffle } from '../../utils/utils';
 
 type Props = {
     title: string;
@@ -8,11 +9,14 @@ type Props = {
 }
 
 const BookInlineContainer:FC<Props> = ({title, bookList} ) => {
+
+  let books = shuffle(bookList)
+
   return (
     <section className='best_sellers_section' >
         <h3>{title}</h3>
         <div className='books_inlineContainer'>
-        {bookList.map((book: BookType) => (
+        {books.map((book: BookType) => (
             <BookCard {...book} key={book.isbn} />
         ))}
         </div>
--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -8,3 +8,10 @@ const currencyHandler = new Intl.NumberFormat(undefined, {
     return currencyHandler.format(number)
   }
   
+
+// for shuffle arrays
+export const shuffle = (array: any[]) => { 
+  return array.map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value); 
+}; 
