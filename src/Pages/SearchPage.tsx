5051e327defb349b74ab37c62d865f31053f9a0e
Enric Broto Hernandez <enrique7siete@gmail.com>
2023-10-10 11:23:32 +0200

    (arrange) searchPage functionality &  styles

--- src/Pages/SearchPage.tsx
+++ src/Pages/SearchPage.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { ChangeEvent } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import { apiContext } from "../context/APIContext";
-
 import { IoArrowBackCircleOutline } from "react-icons/io5"
-
+import BookCard from '../Components/bookCard/BookCard';
+import { BookType } from '../Types/book';
 
 type Props = {}
 
 function SearchPage({ }: Props) {
+    const { apiBooks } = apiContext()
+    const [searchParams, setSearchParams] = useSearchParams()
+    const query = searchParams.get("q") ?? ""
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchParams({ q: e.target.value })
+    }
     return (
         <>
 
@@ -16,10 +23,41 @@ function SearchPage({ }: Props) {
                     <button><IoArrowBackCircleOutline />Home</button>
                 </Link>
             </header>
-            <div className='SearchBar_container'>
-                <input type="search" name="SearchBar" id="SearchBar" placeholder='search' />
+            <div className='searchBar_container'>
+                <input type="search"
+                    placeholder='search'
+                    onChange={handleSearch}
+                    value={query}
+                />
                 <p>lupa</p>
             </div>
+            <section>
+                <h2 className='searchPage_Title'>{`Search by title: ${query}`}</h2>
+                <ul className='searchPage_BooksContainer'>
+                    {(query.length > 1) &&apiBooks.filter(book => {
+                        if(!query) return true
+                        const bookTitle = book.title.toLocaleLowerCase()
+                        return bookTitle.includes(query.toLocaleLowerCase())
+                    })
+                    .map((book: BookType) => (
+                        <BookCard {...book} key={book.isbn} />
+                    ))} 
+                </ul>
+
+                <h2 className='searchPage_Title'>{`Search by author: ${query}`}</h2>
+                <ul className='searchPage_BooksContainer'>
+                    { (query.length > 1) && apiBooks.filter(book => {
+                        if (!query ) return true
+                        const bookTitle = book.author.toLocaleLowerCase()
+                        return bookTitle.includes(query.toLocaleLowerCase())
+                        
+                    })
+                        .map((book: BookType) => 
+                            
+                            <BookCard {...book} key={book.isbn} />
+                        )}
+                </ul>
+            </section>
         </>
     )
 }
